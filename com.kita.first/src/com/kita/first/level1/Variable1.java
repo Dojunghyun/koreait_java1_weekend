24f677b89fbaa46ee13398b2471b3dd3840f2aa2
Dojunghyun <djhdrh0113@gmail.com>
2021-01-24 17:49:08 +0900

    21.01.24 자바 연산자 설명

---
+++ com.kita.first/src/com/kita/first/level1/Variable1.java
@@ -0,0 +1,31 @@
+package com.kita.first.level1;
+
+public class Variable1 {
+	public static void main(String[] args) {
+		char v1 = 'X';
+		
+		byte v2;
+		short v3;
+		int v4 = 34;
+		long v5 = 234234234234l;
+		
+		float v6 = 20.1f;
+		double v7 = 20.1;
+		
+		boolean v8;
+		v8 = true;
+		v8 = false; 
+		
+		// 상수
+		final int MY_MAX_HEIGHT;
+		final String MY_STRING = "ㅎㅇ";
+		MY_MAX_HEIGHT = 176;
+//		MY_MAX_HEIGHT = 177;
+//		MY_STRING = "다른값";
+		
+		// 한 줄 주석
+		/* 여러 줄 주석 
+		            */
+		
+	}
+}
---
+++ com.kita.first/src/com/kita/first/level1/Variable2.java
@@ -0,0 +1,12 @@
+package com.kita.first.level1;
+
+public class Variable2 {
+	public static void main(String[] args) {
+	long v1 = 2432341234l;
+	int v2 = 500_000_000;
+	System.out.println(v2);
+	float v3 = 102.234234f;
+	
+	String v3 = "안녕하세요";
+	}
+}
